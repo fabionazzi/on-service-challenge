5453bff09289663214167bbda73b11599cb3a251
Fabio Nazzi <fabionnazzi@gmail.com>
2025-06-16 00:43:07 -0300

    Update journey creator use case factory

--- app/src/on_service/application/factories/journey_creator_factory.py
+++ app/src/on_service/application/factories/journey_creator_factory.py
@@ -1,6 +1,8 @@
 from on_service.infrastructure.api_client import APIClientFactory
 from on_service.infrastructure.external_services import APIDogFlightEventsProvider
 from on_service.application.use_cases import JourneyCreatorUseCase
+from on_service.domain.services import FlightEventsProvider, PathFinder
+from on_service.domain.filter import JourneyFilterFactory
 
 
 class JourneyCreatorFactory:
@@ -13,4 +15,11 @@ class JourneyCreatorFactory:
         events_provider = APIDogFlightEventsProvider(
             api_client, config.external_api_url
         )
-        return JourneyCreatorUseCase(events_provider)
+        path_finder = PathFinder()
+        filters = []
+        for filter_config in config.filter_config:
+            name = filter_config.name
+            params = filter_config.params
+            filter = JourneyFilterFactory.create(name, **params)
+            filters.append(filter)
+        return JourneyCreatorUseCase(events_provider, path_finder, filters)
