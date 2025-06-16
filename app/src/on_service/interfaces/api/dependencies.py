979500bc25efdb9c3e433df4aeff9fc02f8cd7ee
Fabio Nazzi <fabionnazzi@gmail.com>
2025-06-16 00:43:41 -0300

    Update dependencies

--- app/src/on_service/interfaces/api/dependencies.py
+++ app/src/on_service/interfaces/api/dependencies.py
@@ -1,8 +1,8 @@
 from on_service.configuration import config
-from on_service.application.factories import JourneCreatorFactory
-from on_service.application.use_cases import JourneCreatorUseCase
+from on_service.application.factories import JourneyCreatorFactory
+from on_service.application.use_cases import JourneyCreatorUseCase
 
 
-def get_journeys_creator_use_case() -> JourneCreatorUseCase:
-    journey_creator = JourneCreatorUseCase.create(config)
+def get_journeys_creator_use_case() -> JourneyCreatorUseCase:
+    journey_creator = JourneyCreatorFactory.create(config)
     return journey_creator
