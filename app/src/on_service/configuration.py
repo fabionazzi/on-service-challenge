08685394126308c2b7f4a18e5292c1150e17dcce
Fabio Nazzi <fabionnazzi@gmail.com>
2025-06-16 00:36:46 -0300

    Complete configuration file

--- app/src/on_service/configuration.py
+++ app/src/on_service/configuration.py
@@ -1,13 +1,24 @@
-from pydantic import HttpUrl
-from pydantic_settings import BaseSettings
+from typing import List
+from pydantic import BaseModel, HttpUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class FilterConfiguration(BaseModel):
+    name: str
+    params: dict
 
 
 class Configuration(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="/configuration/.env", env_file_encoding="utf-8"
+    )
+
     external_api_retries: int = 5
     external_api_timeout: int = 10
     external_api_url: HttpUrl = (
         "https://mock.apidog.com/m1/814105-793312-default/flight-events"
     )
+    filter_config: List[FilterConfiguration]
 
 
 config = Configuration()
