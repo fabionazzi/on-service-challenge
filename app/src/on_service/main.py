8af55a5ce69eb246fcc3d43c5739222286d40d35
Fabio Nazzi <fabionnazzi@gmail.com>
2025-06-16 00:45:15 -0300

    Update main file

--- app/src/on_service/main.py
+++ app/src/on_service/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from on_service.interfaces.api.v1.journey import router as journey_router
+from on_service.interfaces.api.v1.endpoints.journey import router as journey_router
 
 app = FastAPI()
 app.include_router(journey_router)
