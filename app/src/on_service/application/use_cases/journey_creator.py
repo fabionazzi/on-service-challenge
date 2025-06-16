1653a0947a765bf7dd28c29ee988f02c00a45e22
Fabio Nazzi <fabionnazzi@gmail.com>
2025-06-16 00:42:33 -0300

    Complete journeys creator use case

--- app/src/on_service/application/use_cases/journey_creator.py
+++ app/src/on_service/application/use_cases/journey_creator.py
@@ -1,12 +1,31 @@
+from datetime import date
 from typing import List
-from on_service.domain.models import FlightEvent, Journey
-from on_service.domain.services import FlightEventsProvider
+from on_service.domain.models import Journey
+from on_service.domain.services import FlightEventsProvider, PathFinder
+from on_service.domain.filter import JourneyFilter
 
 
 class JourneyCreatorUseCase:
 
-    def __init__(self, flight_events_provider: FlightEventsProvider):
+    def __init__(
+        self,
+        flight_events_provider: FlightEventsProvider,
+        path_finder: PathFinder,
+        filters: List[JourneyFilter],
+    ):
         self._flight_events_provider = flight_events_provider
+        self._path_finder = path_finder
+        self._filters = filters
 
-    def create_journeys(self, departure_city: str, arrival_city: str) -> List[Journey]:
+    def create_journeys(
+        self, departure_city: str, arrival_city: str, date: date
+    ) -> List[Journey]:
         events = self._flight_events_provider.get_events()
+        paths = self._path_finder.create_paths(
+            events, departure_city, arrival_city, date
+        )
+        journeys = [Journey(path=path) for path in paths]
+        for filter in self._filters:
+            journeys = filter.filter_journeys(journeys)
+
+        return journeys
